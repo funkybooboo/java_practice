b92c4584b51cf4c68f20764be9815d7f6225c393
funkybooboo <73196352+funkybooboo@users.noreply.github.com>
2022-05-22 18:06:59 -0600

    Add files via upload

---
+++ Quicksort.java
@@ -0,0 +1,74 @@
+import java.util.Random;
+
+public class Quicksort {
+    public static void main(String[] args) {
+        //load up array
+        Random rand = new Random();
+        int[] numbers = new int[10000000];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = rand.nextInt(100000);
+        }
+
+        System.out.println("Before: ");
+        //printArray(numbers);
+
+        quicksort(numbers);
+
+        System.out.println("\nAfter: ");
+        //printArray(numbers);
+
+
+    }
+
+    private static void quicksort(int[] array) {
+        quicksort(array, 0, array.length - 1);
+    }
+
+    private static void quicksort(int[] array, int lowIndex, int highIndex) {
+
+        if (lowIndex >= highIndex) {
+            return;
+        }
+        int pivotIndex = new Random().nextInt(highIndex - lowIndex) + lowIndex;
+        int pivot = array[pivotIndex];
+        swap(array, pivotIndex, highIndex);
+
+        int leftPointer = partition(array, lowIndex, highIndex, pivot);
+
+        quicksort(array, lowIndex, leftPointer - 1);
+        quicksort(array, leftPointer + 1, highIndex);
+    }
+
+    private static int partition(int[] array, int lowIndex, int highIndex, int pivot) {
+        int leftPointer = lowIndex;
+        int rightPointer = highIndex;
+
+        while (leftPointer < rightPointer) {
+            while (array[leftPointer] <= pivot && leftPointer < rightPointer) {
+                leftPointer++;
+            }
+            while (array[rightPointer] >= pivot && leftPointer < rightPointer) {
+                rightPointer--;
+            }
+
+            swap(array, leftPointer, rightPointer);
+
+        }
+        swap(array, leftPointer, highIndex);
+        return leftPointer;
+    }
+
+    private static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+    private static void printArray(int[] array) {
+        for (int j : array) {
+            System.out.println(j);
+        }
+    }
+
+
+}
