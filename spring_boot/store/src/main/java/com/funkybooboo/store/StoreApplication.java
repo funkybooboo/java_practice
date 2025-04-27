ae816c479411a2ee067aef687e9c50f5cd5eba2c
funkybooboo <nate.stott@pm.me>
2025-04-27 19:15:41 -0400

    creating a user

--- spring_boot/store/src/main/java/com/funkybooboo/store/StoreApplication.java
+++ spring_boot/store/src/main/java/com/funkybooboo/store/StoreApplication.java
@@ -1,5 +1,6 @@
 package com.funkybooboo.store;
 
+import com.funkybooboo.store.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -7,8 +8,17 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class StoreApplication {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+//        ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+//        
+//        context.close();
         
-        context.close();
+        var user1 = new User();
+        user1.setName("name");
+        user1.setEmail("email");
+        user1.setPassword("password");
+
+        var user2 = new User(1L, "name", "email", "password");
+
+        var user3 = User.builder().name("name").email("email").password("password").build();
     }
 }
