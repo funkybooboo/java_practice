4fdb87eedb92723f5ec77e4adeb2434c70f1018e
Nate Stott <nate.stott@pm.me>
2025-04-28 21:29:23 -0400

    more entities

---
+++ spring_boot/store/src/main/java/com/funkybooboo/store/entities/Category.java
@@ -0,0 +1,29 @@
+package com.funkybooboo.store.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Byte id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    @Builder.Default
+    private Set<Product> products = new HashSet<>();
+}
