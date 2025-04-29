40dd14f46e7a37ccab2606a816f96f218f790ba4
Nate Stott <nate.stott@pm.me>
2025-04-28 22:01:50 -0400

    clean up

--- spring_boot/store/src/main/java/com/funkybooboo/store/StoreApplication.java
+++ spring_boot/store/src/main/java/com/funkybooboo/store/StoreApplication.java
@@ -1,9 +1,5 @@
 package com.funkybooboo.store;
 
-import com.funkybooboo.store.entities.Address;
-import com.funkybooboo.store.entities.Profile;
-import com.funkybooboo.store.entities.Tag;
-import com.funkybooboo.store.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -11,16 +7,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class StoreApplication {
     public static void main(String[] args) {
-//        ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-//        
-//        context.close();
-
-        var user = User.builder().name("name").email("email").password("password").build();
-        
-        var profile = Profile.builder().bio("bio").build();
-        
-        user.addProfile(profile);
-
-        System.out.println(user);
+        ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
     }
 }
