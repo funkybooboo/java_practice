3b1ca6f42f0bf5513f8e7a97d77df58382d3838e
Nate Stott <nate.stott@pm.me>
2025-04-30 19:46:02 -0400

    @Transactional

--- spring_boot/store/src/main/java/com/funkybooboo/store/StoreApplication.java
+++ spring_boot/store/src/main/java/com/funkybooboo/store/StoreApplication.java
@@ -2,6 +2,7 @@ package com.funkybooboo.store;
 
 import com.funkybooboo.store.entities.User;
 import com.funkybooboo.store.repositories.UserRepository;
+import com.funkybooboo.store.services.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -10,20 +11,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class StoreApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-        var userRepository = context.getBean(UserRepository.class);
-        
-        // save
-//        var user = User.builder().name("Nate").email("nate.stott@pm.me").password("password").build();
-//        userRepository.save(user);
-        
-        // get by id
-//        var user = userRepository.findById(1L).orElseThrow();
-//        System.out.println(user);
-        
-        // get all
-//        userRepository.findAll().forEach(System.out::println);
-        
-        // delete by id
-//        userRepository.deleteById(1L);
+        var service = context.getBean(UserService.class);
+        service.showEntityStates();
     }
 }
