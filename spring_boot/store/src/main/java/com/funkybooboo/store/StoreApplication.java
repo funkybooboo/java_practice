8713356c51ee29e6c5796d1465676754047602f9
Nate Stott <nate.stott@pm.me>
2025-04-30 19:27:44 -0400

    play with reposiroies and hibernate

--- spring_boot/store/src/main/java/com/funkybooboo/store/StoreApplication.java
+++ spring_boot/store/src/main/java/com/funkybooboo/store/StoreApplication.java
@@ -1,5 +1,7 @@
 package com.funkybooboo.store;
 
+import com.funkybooboo.store.entities.User;
+import com.funkybooboo.store.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -8,5 +10,20 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class StoreApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+        var userRepository = context.getBean(UserRepository.class);
+        
+        // save
+//        var user = User.builder().name("Nate").email("nate.stott@pm.me").password("password").build();
+//        userRepository.save(user);
+        
+        // get by id
+//        var user = userRepository.findById(1L).orElseThrow();
+//        System.out.println(user);
+        
+        // get all
+//        userRepository.findAll().forEach(System.out::println);
+        
+        // delete by id
+//        userRepository.deleteById(1L);
     }
 }
